754c5cb4e3b816a2c0236ff108118af229a9cf87
thomasfregene <fregzythomas@gmail.com>
2020-08-12 18:06:49 +0100

    creating the components and referencing to the main file -App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">column number one</div>
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6"><span><i className="fas fa-home" aria-hidden="true"></i></span></div>
-      </div>
-    </div>
+   <React.Fragment>
+     <Navbar/>
+     <ProductList/>
+     <Details/>
+     <Default/>
+   </React.Fragment>
   );
 }
 
