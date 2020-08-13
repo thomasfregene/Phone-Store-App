f130599f214e8fa3c0c9e2576c07b26d527db72e
thomasfregene <fregzythomas@gmail.com>
2020-08-13 02:58:56 +0100

    using styled components -navbar.js, button.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
                 {/*https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported);
                 https://www.iconfinder.com/Makoto_msk*/}
                 <Link to="/">
@@ -19,14 +22,23 @@ class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
+                    <ButtonContainer>
                     <span><i className="fas fa-cart-plus"></i></span>
                     my cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+`
 
 export default Navbar;
