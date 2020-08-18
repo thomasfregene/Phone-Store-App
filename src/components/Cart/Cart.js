f12e79503e1c9382591e31e21b42775bc6d07dac
thomasfregene <fregzythomas@gmail.com>
2020-08-18 03:28:42 +0100

    working on the cart component -added the empty cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import {ProductConsumer} from '../../context'
 
 class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart"/>
-                <CartColumns></CartColumns>
+                <ProductConsumer>
+                    {value=>{
+                        const {cart} = value;
+                        if(cart.length > 0){
+                            return(
+                                <>
+                                <Title name="your" title="cart"/>
+                                <CartColumns/>
+                                </>
+                            )
+                        }else{
+                            return <EmptyCart/>
+                        }
+                       
+                    }}
+                </ProductConsumer>
+                
+                
             </section>
         )
     }
