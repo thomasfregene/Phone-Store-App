916ad2e789547b207c38a2c1c0c86291eb9bd9ab
thomasfregene <fregzythomas@gmail.com>
2020-08-19 01:58:55 +0100

    styling the cartitem component and added a carttotal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
-import {ProductConsumer} from '../../context'
+import {ProductConsumer} from '../../context';
+import CartList from './CartList';
+import CartTotals from './CartTotals'
 
 class Cart extends Component {
     render() {
@@ -16,6 +18,8 @@ class Cart extends Component {
                                 <>
                                 <Title name="your" title="cart"/>
                                 <CartColumns/>
+                                <CartList value={value}/>
+                                <CartTotals value={value}/>
                                 </>
                             )
                         }else{
